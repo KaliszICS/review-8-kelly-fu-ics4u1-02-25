f8d417850c2453d2f51c85d898020152c3815a93
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-02-18 15:21:09 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,6 +2,47 @@ public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+}
+	public static String pluralize(String word){
+		if (word.endsWith("y") && !word.endsWith("ey")){
+		return "ies";
+		}
+		else if (word.endsWith("ey")){
+		return "eys";
+		}
+		else if (word.endsWith("ife")){
+			return "ives";
+		}
+		else{
+			return "s";
+		}
 	}
+
+	public static String min(int num1, int num2, int num3){
+		if (num)
+	}
+
+
+
+	public static String isLeapYear(int year){
+		if (year % 4 == 0){
+			if (year % 100 == 0){
+				if (year % == 400){
+					return "true";
+				}
+				else{
+					return "false";
+				}
+			}
+			else{
+				return "false";
+			}
+		}
+		else{
+			return "false";
+		}
+	}
+
+	
 
 }
