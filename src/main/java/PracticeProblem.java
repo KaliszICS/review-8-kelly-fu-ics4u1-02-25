69298f20e2deadf5ab4e8fd05916c04c51908442
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-02-19 05:30:34 +0000

    update but get 1 wrong

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,42 +4,42 @@ public class PracticeProblem {
 
 }
 	public static String pluralize(String word){
-		if (word.endsWith("y") && !word.endsWith("ey")){
+		if (word.endsWith("y")&& !word.endsWith("ey")){
 		return "ies";
 		}
 		else if (word.endsWith("ey")){
-		return "eys";
+			return "eys";
 		}
 		else if (word.endsWith("ife")){
 			return "ives";
 		}
-		else{
+		else {
 			return "s";
 		}
 	}
 
-	public static String min(int num1, int num2, int num3){
-		if (num)
+	public static int min(int num1, int num2, int num3){
+		return Math.min(num1, Math.min(num2, num3));
 	}
 
 
 
-	public static String isLeapYear(int year){
+	public static boolean isLeapYear(int year){
 		if (year % 4 == 0){
 			if (year % 100 == 0){
-				if (year % == 400){
-					return "true";
+				if (year % 400 == 0){
+					return true;
 				}
 				else{
-					return "false";
+					return false;
 				}
 			}
 			else{
-				return "false";
+				return true;
 			}
 		}
 		else{
-			return "false";
+			return false;
 		}
 	}
 
